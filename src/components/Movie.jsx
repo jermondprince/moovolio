97a619431153c2634172076be6c06854bee6ddf2
jermondprince <jermondprince@gmail.com>
2023-03-05 13:38:56 -0600

    Added movie description when clicked on movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,9 +7,14 @@ import { useNavigate } from "react-router-dom";
 const Movie = ({ movie, user }) => {
   const [like, setLike] = useState(false);
   const [saved, setSaved] = useState(false);
+  const [isActive, setIsActive] = useState(false);
   const navigate = useNavigate();
 
   const movieID = doc(db, "users", `${user?.email}`);
+
+  const handleActive = () => {
+    setIsActive(!isActive);
+  };
 
   const saveShow = async () => {
     if (user?.email) {
@@ -29,7 +34,7 @@ const Movie = ({ movie, user }) => {
   };
   return (
     <>
-      <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] h-full cursor-pointer relative p-2 inline-block">
+      <div className="w-[240px] sm:w-[260px] md:w-[300px] lg:w-[340px] h-full  cursor-pointer relative p-2 inline-block">
         <img
           className="w-full h-auto block"
           src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
@@ -37,8 +42,21 @@ const Movie = ({ movie, user }) => {
         />
 
         <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-          <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+          <p
+            onClick={handleActive}
+            className={`whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center ${
+              isActive === true ? "hidden" : ""
+            }`}
+          >
             {movie?.title}
+          </p>
+          <p
+            onClick={handleActive}
+            className={`whitespace-normal text-[10px] md:text-sm font-bold h-full text-center flex justify-center items-center ${
+              isActive === false ? "hidden " : ""
+            }`}
+          >
+            {movie?.overview}
           </p>
           <p onClick={saveShow}>
             {like ? (
